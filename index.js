21ef8b6c3528da53406cd7411fd16c5217524873
Moeraf <mouemenrafei12@icloud.com>
2022-09-15 13:37:08 +0300

    third change

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-var services = document.getElementById("services");
-var content =`<div>
+var services = document.getElementById('Services');
+var Original_Content = `<div>
 <div class="icon"><i class="bi bi-briefcase"></i></div>
-<h4 class="title"><a href="">Lorem Ipsum</a></h4>
+<h4 class="title"><a href="">TITLE</a></h4>
 <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
 </div>`
 
-for (let i = 0; i < 6; i++) {
-    var service = document.createElement(`div`)
-    service.innerHTML = content
-    service.className = 'col-lg-4 col-md-6 icon-box'
-    services.appendChild(service)
-}
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(json => {
+    json.forEach(element => {
+        var content = Original_Content;
+        content = content.replace('TITLE',element.name)
+        var service = document.createElement('div')
+        service.innerHTML = content
+        service.className = "col-lg-4 col-md-6 icon-box"
+        services.appendChild(service)
+    });
+  })
