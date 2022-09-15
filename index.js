1bb9db13105cabe25a05d38210f41af761990405
Moeraf <mouemenrafei12@icloud.com>
2022-09-15 13:41:27 +0300

    wala shi

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var services = document.getElementById('Services');
 var Original_Content = `<div>
 <div class="icon"><i class="bi bi-briefcase"></i></div>
-<h4 class="title"><a href="">TITLE</a></h4>
+<h4 class="title"><a href="">TITLES</a></h4>
 <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
 </div>`
 
@@ -10,7 +10,7 @@ fetch('https://jsonplaceholder.typicode.com/users')
   .then(json => {
     json.forEach(element => {
         var content = Original_Content;
-        content = content.replace('TITLE',element.name)
+        content = content.replace('TITLES',element.name)
         var service = document.createElement('div')
         service.innerHTML = content
         service.className = "col-lg-4 col-md-6 icon-box"
