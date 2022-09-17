0645fef637586d2de72a1d109e1be9d8c38340db
Moeraf <mouemenrafei12@icloud.com>
2022-09-18 00:07:18 +0300

    change for heroku

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ var services = document.getElementById('Services');
 var Original_Content = `<div>
 <div class="icon"><i class="bi bi-briefcase"></i></div>
 <h4 class="title"><a href="">TITLES</a></h4>
-<p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
+<p class="description">DESCRIPTION</p>
 </div>`
 
-fetch('https://jsonplaceholder.typicode.com/users')
+fetch('http://localhost:3000/services')
   .then(response => response.json())
   .then(json => {
-    json.forEach(element => {
+    json.services.forEach(element => {
         var content = Original_Content;
-        content = content.replace('TITLES',element.name)
+        content = content.replace('TITLES',element.A)
+        content = content.replace('DESCRIPTION',element.B)
         var service = document.createElement('div')
         service.innerHTML = content
         service.className = "col-lg-4 col-md-6 icon-box"
